497280f0d18fba4cfbf39c713658bf54b0ee0a96
Vivek Saurabh <saurabhvivek30@gmail.com>
2023-09-12 00:38:05 +0530

    dicom upload

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Azure;
 using NHapi.Model.V23.Datatype;
 using NHapi.Model.V23.Message;
 using NHapi.Base.Parser;
+using FellowOakDicom;
+using FellowOakDicom.Log;
 
 class Program
 {
@@ -14,23 +16,29 @@ class Program
         var connectionString = "DefaultEndpointsProtocol=https;AccountName=nhapi;AccountKey=UkFUH4QZR6TQvr+IugS1slIpke0VJzmOsscKyFgtFvYEalLvGUwmomb/diCL8VSCFa/3D29VSBcs+ASt81gOwQ==;EndpointSuffix=core.windows.net";
         var BlobServiceClient = new BlobServiceClient(connectionString);
         var ContainerClient = BlobServiceClient.GetBlobContainerClient("nhapi");
-
+        
+        //hl7
         var _oruR01Message = CreateORU_R01_Message();
 
-        var path = "";
+        var hl7path = "";
         var submitter = "radiology";
         var someID = "slkafjlkjsldjk";
 
         if(submitter == "radiology")
         {
-            path = $"radiology/path/{someID}";
+            hl7path = $"radiology/path/{someID}";
         }
         else
         {
-            path = $"nonRadiology";
+            hl7path = $"nonRadiology";
         }
+        ContainerClient.DeleteBlob(hl7path);
+        ContainerClient.UploadBlob(hl7path, BinaryData.FromString(parser.Encode(_oruR01Message)));
 
-        ContainerClient.UploadBlob(path, BinaryData.FromString(parser.Encode(_oruR01Message)));
+        //dicom
+        var dicompath = "dicom/path";
+        var dicomFile = CrateDicomFile();
+        ContainerClient.UploadBlob(dicompath, BinaryData.FromString(dicomFile.WriteToString()));
     }
 
     public static ORU_R01 CreateORU_R01_Message()
@@ -50,5 +58,10 @@ class Program
         mshSegment.ProcessingID.ProcessingID.Value = "P";
 
         return _oruR01Message;
+    }
+
+    public static DicomFile CrateDicomFile()
+    {
+        return DicomFile.Open("0002.DCM");
     }
 }
